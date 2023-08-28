5ce474ba83fb67bed5fba6471ed1ebe90008c0fe
Aysenur Aydın <aysenuraydn08@gmail.com>
2023-08-28 11:35:13 +0300

    fix: image sources fix for vercel

--- src/data.js
+++ src/data.js
@@ -1,36 +1,36 @@
 const dogsData = [
-    {
-        name: "Alex",
-        avatar: "/public/dog-alex.jpg",
-        age: 28,
-        bio: "Exercise is my life. Playful.",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Rex",
-        avatar: "/public/dog-rex.jpg",
-        age: 25,
-        bio: "Art. Literature. Natural wine. Yoga.",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Bella",
-        avatar: "/public/dog-bella.jpg",
-        age: 43,
-        bio: "Yup, that's my owner. U can meet him if you want",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Teddy",
-        avatar: "/public/dog-teddy.jpg",
-        age: 30,
-        bio: "How you doin?",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    }
-]
+  {
+    name: 'Alex',
+    avatar: '/dog-alex.jpg',
+    age: 28,
+    bio: 'Exercise is my life. Playful.',
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: 'Rex',
+    avatar: '/dog-rex.jpg',
+    age: 25,
+    bio: 'Art. Literature. Natural wine. Yoga.',
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: 'Bella',
+    avatar: '/dog-bella.jpg',
+    age: 43,
+    bio: "Yup, that's my owner. U can meet him if you want",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: 'Teddy',
+    avatar: '/dog-teddy.jpg',
+    age: 30,
+    bio: 'How you doin?',
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+];
 
-export { dogsData }
+export { dogsData };
--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ rewindBtn.addEventListener('click', () => {
 
 function renderBadge(badgeName) {
     document.getElementById('badge-container').innerHTML = `
-        <img src="/public/badge-${badgeName}.png">
+        <img src="/badge-${badgeName}.png">
     `
 }
 
